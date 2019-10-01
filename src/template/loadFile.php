3527cd7d4475c11fb79d86d4d7cf6e32de1e5c65
nata <korovina-nataliya@mail.ru>
2019-10-01 17:45:26 +0300

    start work

---
+++ src/template/loadFile.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<input type="file" multiple="multiple" accept=".txt,image/*">
+<a href="#" class="upload_files button">Загрузить файлы</a>
+<div class="ajax-reply"></div>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</body>
+</html>
